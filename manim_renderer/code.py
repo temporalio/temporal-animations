b8d24ceffd0913c6f17fa572abfdd411fb2945a3
Dan Davison <dan.davison@temporal.io>
2023-12-31 11:12:24 -0500

    Fix code display

--- manim_renderer/code.py
+++ manim_renderer/code.py
@@ -1,6 +1,6 @@
 from typing import Generic, TypeVar
 
-from manim import BLACK, DOWN, LEFT, PINK, Arrow, Code, Mobject, VGroup
+from manim import DOWN, LEFT, PINK, Arrow, Code, Mobject, VGroup
 
 import tempyral
 from manim_renderer.entity import ProxyEntity
@@ -15,9 +15,9 @@ class ProxyEntityWithCode(ProxyEntity, Generic[E]):
             code=entity.code,
             language=entity.language,
             insert_line_no=False,
+            background_stroke_width=1,
             font_size=FONT_SIZE_CODE,
             font=FONT_CODE,
-            background_stroke_width=0,
             line_spacing=0.7,
         ).to_edge(LEFT, buff=0.1)
         lines = code[2]
@@ -26,13 +26,13 @@ class ProxyEntityWithCode(ProxyEntity, Generic[E]):
                 Arrow(
                     start=line.get_edge_center(LEFT) + LEFT,
                     end=line.get_edge_center(LEFT),
-                    color=BLACK,
                 )
+                .set_opacity(0)
                 .next_to(line, LEFT, buff=0.1)
                 .shift(DOWN * 0.025)
                 for line in lines
             )
         )
         for line_num in entity.blocked_expressions:
-            arrows[line_num - 1].set_color(PINK)
+            arrows[line_num - 1].set_color(PINK).set_opacity(1)
         return VGroup(code, arrows)
