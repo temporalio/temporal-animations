cc37ce471b0b64b191e74a18ef3f5a25f865421f
Dan Davison <dan.davison@temporal.io>
2024-01-03 10:16:45 -0500

    Revert "Sort simulation events by Lamport time"
    This reverts commit bec6ee482b60a157c2153fc3889810d619a3d4df.

--- manim_renderer/event_processor.py
+++ manim_renderer/event_processor.py
@@ -39,14 +39,6 @@ async def process_simulation_events():
     _render_simulation_events(events)
 
 
-def lamport_time(event: Event) -> int:
-    match event:
-        case StateChangeEvent(entity):
-            return entity.time
-        case MessageEvent(_, _, msg_entity):
-            return msg_entity.time
-
-
 def _render_simulation_events(
     events: list[StateChangeEvent[tempyral.Entity] | MessageEvent[tempyral.Entity]],
 ):
@@ -54,7 +46,7 @@ def _render_simulation_events(
     animations: list[Iterable[Animation | None]] = []
     serial = True
     n = 400
-    for event in sorted(events, key=lamport_time):
+    for event in events:
         match event:
             case StateChangeEvent(entity):
                 proxy_entity = proxy_entity_registry.get(entity)
