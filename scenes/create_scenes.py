eb63dcb4b7bceaa0c18ade69a4585ced9f80a30c
Dan Davison <dan.davison@temporal.io>
2024-01-02 23:15:37 -0500

    Parallelize animation creation

--- scenes/create_scenes.py
+++ scenes/create_scenes.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 from pathlib import Path
+from threading import Thread
 
 SCENES = [
     "ExecuteWorkflow",
@@ -9,18 +10,32 @@ SCENES = [
 ]
 
 
-def create_animation(scene):
+def create_animation(scene, output_to_file=False):
     scenes_dir = Path(__file__).parent
     scene_file = scenes_dir / f"{scene}.py"
     assert scene_file.exists(), scene_file
-    subprocess.run(["manim", "-qh", scene_file, scene], cwd=scenes_dir)
+    cmd = ["manim", "-qh", scene_file, scene]
+    if output_to_file:
+        with open(f"/tmp/{scene}.out", "w") as out_file, open(
+            f"/tmp/{scene}.err", "w"
+        ) as err_file:
+            subprocess.run(cmd, cwd=scenes_dir, stdout=out_file, stderr=err_file)
+    else:
+        subprocess.run(cmd, cwd=scenes_dir)
 
 
-def create_animations(scenes: list[str]):
-    for scene in scenes:
-        create_animation(scene)
+def create_animations_parallel(scenes: list[str]):
+    threads = [Thread(target=create_animation, args=[s, True]) for s in scenes]
+    for t in threads:
+        t.start()
+    for t in threads:
+        t.join()
 
 
 if __name__ == "__main__":
     scenes = sys.argv[1:] or SCENES
-    create_animations(scenes)
+    if False:
+        create_animations_parallel(scenes)
+    else:
+        for scene in scenes:
+            create_animation(scene, output_to_file=False)
