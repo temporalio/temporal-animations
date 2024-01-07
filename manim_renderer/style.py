fed837f6cb441a1bc9c0ebaf29fcf69394aa7263
Dan Davison <dan.davison@temporal.io>
2024-01-07 13:58:51 -0500

    Opaque messages

--- manim_renderer/style.py
+++ manim_renderer/style.py
@@ -1,4 +1,13 @@
-from manim import BLUE_E, ORANGE, Line, Mobject, Text
+from manim import (
+    BLUE_E,
+    ORANGE,
+    Line,
+    ManimColor,
+    Mobject,
+    SurroundingRectangle,
+    Text,
+    VGroup,
+)
 from manim.typing import Point3D
 
 FONT_MONOSPACE = "Monaco"  # Monaco, Menlo, PT Mono
@@ -19,14 +28,20 @@ STROKE_OPACITY_PENDING_REQUEST_RAY = 0.7
 BUFF_PENDING_REQUEST = 0.5
 
 
-def message(name: str, **kwargs) -> Mobject:
-    return Text(
+def message(name: str) -> Mobject:
+    text = Text(
         name,
         font=FONT_MESSAGE,
         font_size=FONT_SIZE_MESSAGE,
         color=COLOR_MESSAGE,
-        **kwargs
     )
+    rect = SurroundingRectangle(
+        text,
+        stroke_width=0,
+        fill_opacity=1,
+        fill_color=COLOR_SCENE_BACKGROUND,
+    )
+    return VGroup(rect, text)
 
 
 def pending_request_ray(start: Point3D, end: Point3D) -> Mobject:
@@ -44,21 +59,20 @@ def invisible_message() -> Mobject:
     return Text(".").set_opacity(0)
 
 
-def actor(name: str, **kwargs) -> Mobject:
-    return Text(name, font=FONT_ACTOR, font_size=FONT_SIZE_ACTOR, **kwargs)
+def actor(name: str) -> Mobject:
+    return Text(name, font=FONT_ACTOR, font_size=FONT_SIZE_ACTOR)
 
 
-def history_event(name: str, **kwargs) -> Mobject:
+def history_event(name: str, color: ManimColor) -> Mobject:
     return Text(
-        name, font=FONT_HISTORY_EVENT, font_size=FONT_SIZE_HISTORY_EVENT, **kwargs
+        name, font=FONT_HISTORY_EVENT, font_size=FONT_SIZE_HISTORY_EVENT, color=color
     )
 
 
-def pending_update(name: str, **kwargs) -> Mobject:
+def pending_update(name: str) -> Mobject:
     return Text(
         name,
         font=FONT_HISTORY_EVENT,
         font_size=FONT_SIZE_HISTORY_EVENT,
         color=COLOR_PENDING_UPDATE,
-        **kwargs
     )
